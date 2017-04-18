65b3686909dec3864237c0dcb24e04fc53f35c30
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-04-18 22:33:10 +0900

    initial code commit. most of codes are copied from OpenCV 3.2 (so this repo has the same licence to OpenCV).

---
+++ src/modules/dnn/opencl_kernels_dnn.hpp
@@ -0,0 +1,10 @@
+#ifndef __OPENCV_DNN_OPENCL_KERNELS_DNN_HPP__
+#define __OPENCV_DNN_OPENCL_KERNELS_DNN_HPP__
+
+// This file is workaround and has to do nothing if HAVE_OPENCL is not defined
+
+#ifdef HAVE_OPENCL
+#error "undefine HAVE_OPENCL"
+#endif
+
+#endif
